9695770b004b081604222227e7ef06f7f4fc64b2
julianborrey <julian.borrey@duke.edu>
2013-08-04 02:25:17 +1000

    first commit, blank ruby app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Site for Online Debating Tabbing #
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TabSite::Application.config.secret_key_base = 'bf9308240371cf0ad11f11867d419f1c89130e8d5b95b09a7ff464d6699005402b09cd0e4f849402afbc2b7da644a6afe1ddcd5280c1a43f83f4889a8803ad30'
